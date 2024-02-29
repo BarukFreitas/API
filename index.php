407a71484cf2f817f76c1c9db31312b81b30cff0
Baruk Freitas <baruk.freitas@gmail.com>
2024-02-28 22:27:46 -0300

    Update

--- index.php
+++ index.php
@@ -1,28 +1,53 @@
 <?php
 
-    //TOKEN - token de segurança para validar e saber o que estamos chamando a api
-    //AÇÃO - o que vamos fazer
-    //ID - ID do cliente
-    //VALOR - nome do cliente ou atualização do cliente
-
     define('TOKEN', 'meutoken');
 
     if(isset($_GET['token'])){
         $token = $_GET['token'];
         if($token == TOKEN){
-            //PODEMOS CONTINUAR NA API, TEMOS ACESSO
+
 
             if(isset($_GET["acao"])) {
+                $pdo = new PDO('mysql:host=localhost;dbname=API_Curso','root','bakanoyuba');
                 $acao = $_GET['acao'];
 
                 if($acao == 'novo_contato'){
-                    die(json_encode(array('sucesso'=>true)));
-                }else if($acao == 'deletar_contato'){
+                    $nome = isset($_GET['nome']) ? $_GET['nome'] : '';
+                    $sql = $pdo->prepare('INSERT INTO `clientes` VALUES (null,?)');
+                    if($sql->execute(array($nome))){
 
+                        die(json_encode(array('sucesso'=>true,'inserido'=>$nome)));
+                    }else{
+                        die(json_encode(array('sucesso'=>false,'erro'=>'Não foi possível inserir seu contato')));
+                    };
+                    
+                }else if($acao == 'deletar_contato'){
+                    if(!isset($_GET['id'])){
+                        die(json_encode(array('erro'=>'Precisamos de um ID.')));    
+                    };
+                    $id = (int)$_GET['id']; 
+                    $sql = $pdo->prepare('DELETE FROM `clientes` WHERE id = ?');
+                    if($sql->execute(array($id))){
+                        die(json_encode(array('sucesso'=>true,'deletado'=>$id)));
+                    }else{
+                        die(json_encode(array('sucesso'=>false,'erro'=>'Não foi possivel deletar')));
+                    }
                 }else if($acao == 'atualizar_contato'){
 
                 }else if($acao == 'visualizar_contato'){
+                    if(!isset($_GET['id'])){
+                        die(json_encode(array('erro'=>'Precisamos de um ID valido para mostrar.')));    
+                    };
+                    $id = (int)$_GET['id'];
+                    $sql = $pdo->prepare('SELECT * FROM `clientes` WHERE id = ?');
+                    $sql = execute(array($id));
 
+                    if($sql->rowCount() >=1){
+                        $dados = $sql->fetch();
+                        die(json_encode($dados));
+                    }else{
+                        die('Não encontramos nenhum usuário com este ID.');
+                    }
                 }else{
                     die('A ação não é valida em nosso sistema de API.');
                 }
